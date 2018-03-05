b5632a18e314b70e344fc9988c7b1a9efb8b3df5
Bernal Gomez <d_bernalgomez@tss-hr420-dm16.local>
2018-03-05 13:07:29 -0500

    fetch database

--- public/javascripts/formhandler.js
+++ public/javascripts/formhandler.js
@@ -1,4 +1,22 @@
 (() =>{
   //cms handler load
-  console.log('cms handler loaded'); 
-});
+  console.log('cms handler loaded');
+//variables goes top
+  let submitButton = document.querySelector('.add-data'),
+  targetForm = document.querySelector('form');
+//functionality goes here
+  function addRecord(){
+    event.preventDefault();
+
+    var formData = new  FormData(targetForm);
+    strData = {};
+
+    for (var [key,value] of formData.entries()){
+      console.log(key,value);
+
+      strData[key] = value;
+    }
+  }
+  //events listeners
+  submitButton.addEventListener('click', addRecord);
+})();
