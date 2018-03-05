da0f9a22d2a5534b416cfbc3c5b59581c05e36c1
Bernal Gomez <d_bernalgomez@tss-hr420-dm16.local>
2018-03-05 13:57:13 -0500

    final files

--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,24 @@ var toRender = (config.kidsmode) ? 'main_kids' : 'home';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render(toRender, {
-    title: 'Done yet?',
+  res.render('home', {
+    title: 'Kids Page',
     message : "handlebars is awesome",
     mainpage : true,
+    cms : false,
     kidsmode : config.kidsmode
   });
 });
 
+router.get('/cms', (req , res) => {
+  console.log('init the cms route');
+  res.render('cmsform', {
+  cms : true,
+  mainpage : false
+});
+});
+
 module.exports = router;
+
+
+// hit routes with main page and other pages
