52b7daedfc1b985679688b99a15835706c2a3fe2
diegob08 <d.bernal91@gmail.com>
2018-03-07 21:42:42 -0500

    track lock file

--- app.js
+++ app.js
@@ -10,14 +10,18 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', xhb({ defaultLayout : 'main'}));
+app.engine('handlebars', xhb({
+  defaultLayout: 'main'
+}));
 app.set('view engine', 'handlebars');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
