b9fae0c2c9bf92a26fb05b37a91152b5ca987904
diegob08 <d.bernal91@gmail.com>
2018-03-07 21:42:22 -0500

    update mysql for windows

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {
   user : "root",
-  password : "root", // leave this blank for windows machines
-  port: 8889,
+  password : "", // leave this blank for windows machines
+  port: 3306,
   host: "localhost",
   database: "cooperStuff",
 
