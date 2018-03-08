2d6d4df79357833af6b38d534b9e8e32f065efda
diegob08 <d.bernal91@gmail.com>
2018-03-07 23:11:33 -0500

    connect to database to fetch data

--- routes/category.js
+++ routes/category.js
@@ -5,25 +5,20 @@ var router = express.Router();
 /* GET Category page. */
 router.get('/:category', (req, res) => {
   var category = req.params.category;
+  connect.query('SELECT * FROM video WHERE category = ?', [category], (err, result) => {
+    if (err) {
+      throw err;
+      console.log(err);
+    } else {
+      console.log(result);
 
-  res.render('category', {
-    title: 'Category ' + category,
-    message: "This is a list for category: " + category,
+      res.render('category', {
+        title: 'Category ' + category,
+        message: "This is a list for category: " + category,
+        videoData: result
+      });
+    }
   });
-  // connect.query('SELECT * FROM mainmodel', (err, result) => {
-  //   if (err) {
-  //     throw err;
-  //     console.log(err);
-  //   } else {
-  //     console.log(result);
-  //
-  //     res.render('cars', {
-  //       title: 'Mini Cars',
-  //       message: "A Selection of Minis",
-  //       carData: result
-  //     });
-  //   }
-  // });
 });
 
 module.exports = router;
