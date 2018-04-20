568032f29bf86f01f782c60786e4799399aaed93
diegob08 <d.bernal91@gmail.com>
2018-04-19 21:26:54 -0400

    use new db

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 var config = {
   user : "root",
-  password : "root", // leave this blank for windows machines
-  port: 8889,
+  password : "", // leave this blank for windows machines
+  port: 3306,
   host: "localhost",
-  database: "cooperstuff",
+  database: "db_videos",
 
   kidsmode : true
 }
